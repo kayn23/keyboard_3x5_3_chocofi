fb75bdb59a9c55269dfc77deca04209a087fcd9c
Chernyshev Anatoly <42699383+kayn23@users.noreply.github.com>
2024-01-14 01:32:54 +0300

    init

---
+++ keymaps/choco_one/keymap.c
@@ -0,0 +1,256 @@
+#include QMK_KEYBOARD_H
+#include "quantum.h"
+#include <stdio.h>
+#include <stdint.h>
+#include "strash_keymap.h"
+
+// screenshot to buffer
+#define SCR_TO_B SCMD(LCTL(KC_4))
+// screenshot to file
+#define SCR_TO_F SCMD(KC_4)
+// screen recording to file
+#define VIDEO SCMD(KC_5)
+// change language
+// #define LANG HYPR(KC_SPC)
+#define LANG C(KC_SPC)
+
+enum layout {
+	QWE = 0,
+	NAV,
+	SYM
+};
+
+enum key {
+    BASE = SAFE_RANGE, // move to active base layer
+    HIS_BACK,          // go back in the history (browser)
+    HIS_FORW,          // go forward in the history (browser)
+    PREV_TAB,          // go to the previous tab
+    NEXT_TAB,          // go to the next tab
+    PREV_APP,          // go to the previous app
+    NEXT_APP,          // go to the next app
+    PREW_SPC,          // go to the previous space
+    NEXT_SPC,          // go to the next space
+    WEB_INSPECTOR,     // open/close web inspector
+    T_BASE_H_NAV,      // change base layer and lang on tap and open nav layer on hold
+    T_MEDIA_H_SYM      // open media layer on tap and open sym layer on hold
+};
+
+bool is_select_prev_app_active = false;
+bool is_select_next_app_active = false;
+bool esc_and_enter_handle = true;
+
+uint16_t timer_base_nav;
+uint16_t timer_media_sym;
+
+enum tap_dance {
+	QWE_P_QUOT,
+	ESC_LANG
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+	[QWE_P_QUOT] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_QUOT),
+	// [ESC_LANG] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, LANG)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+
+    // йцуке нгшщз(э)
+    // фывап ролдж
+    // ячсми тьбю/
+
+    [QWE] = LAYOUT_split_3x5_3( \
+	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+	            KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O, TD(QWE_P_QUOT), \
+	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+	    LCTL_T(KC_A), LOPT_T(KC_S), LSFT_T(KC_D), LCMD_T(KC_F),         KC_G,            KC_H, RCMD_T(KC_J), RSFT_T(KC_K), ROPT_T(KC_L),RCTL_T(KC_SCLN), \
+	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+	            KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,         KC_M,      KC_COMM,       KC_DOT,        KC_SLSH, \
+	//|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+								//   TD(ESC_LANG), T_BASE_H_NAV,       KC_ENT,  RCMD_T(KC_SPC),T_MEDIA_H_SYM,      KC_BSPC 
+								          KC_ESC, T_BASE_H_NAV,       KC_ENT,  RCMD_T(KC_SPC),T_MEDIA_H_SYM,      KC_BSPC \
+								//|-------------+-------------+-------------|  |-------------+-------------+-------------|
+	),
+
+
+    [NAV] = LAYOUT_split_3x5_3( \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		        HIS_BACK,     HIS_FORW,     PREV_TAB,     NEXT_TAB,LCMD(KC_BSPC),    RSG(KC_LEFT),RSFT(KC_LEFT),RSFT(KC_DOWN),  RSFT(KC_UP),  RSFT(KC_RGHT), \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		  LCTL_T(KC_TAB),      KC_LOPT,      KC_LSFT,      KC_LCMD,LOPT(KC_BSPC),   RCMD(KC_LEFT),      KC_LEFT,      KC_DOWN,        KC_UP,        KC_RGHT, \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		        PREV_APP,     NEXT_APP,     PREW_SPC,     NEXT_SPC,WEB_INSPECTOR,    RSA(KC_LEFT),ROPT(KC_LEFT),ROPT(KC_DOWN),  ROPT(KC_UP),  ROPT(KC_RGHT), \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+										TD(ESC_LANG),        KC_NO,       KC_ENT,          KC_SPC,        KC_NO,      KC_BSPC \
+									//|-------------+-------------+-------------|  |-------------+-------------+-------------|
+        ),
+
+    [SYM] = LAYOUT_split_3x5_3( \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		         KC_EXLM,        KC_AT,      KC_HASH,       KC_DLR,      KC_PERC,         KC_CIRC,      KC_AMPR,      KC_ASTR,      KC_LPRN,        KC_RPRN, \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		   LCTL_T(KC_P1),LOPT_T(KC_P2),LSFT_T(KC_P3),LCMD_T(KC_P4),        KC_P5,           KC_P6,RCMD_T(KC_P7),RSFT_T(KC_P8),ROPT_T(KC_P9),  RCTL_T(KC_P0), \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+		         KC_LCBR,      KC_RCBR,      KC_MINS,       KC_EQL,       KC_GRV,         KC_SLSH,      KC_LBRC,      KC_RBRC,      KC_PIPE,        KC_BSLS, \
+        //|-------------+-------------+-------------+-------------+-------------|  |-------------+-------------+-------------+-------------+---------------|
+										TD(ESC_LANG),        KC_NO,       KC_ENT,          KC_SPC,        KC_NO,      KC_BSPC \
+									//|-------------+-------------+-------------|  |-------------+-------------+-------------|
+        ),
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return state;
+};
+
+void unselect_app_selection(void) {
+	esc_and_enter_handle = true;
+    if (is_select_prev_app_active == true) {
+        is_select_prev_app_active = false;
+		esc_and_enter_handle = false;
+		unregister_mods(MOD_MASK_SG);
+    }
+    if (is_select_next_app_active == true) {
+        is_select_next_app_active = false;
+		esc_and_enter_handle = false;
+		unregister_mods(MOD_MASK_GUI);
+    }
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case T_BASE_H_NAV:
+            if (record->event.pressed) {
+                timer_base_nav = timer_read();
+                if (IS_LAYER_OFF(NAV)) layer_on(NAV);
+            } else {
+                if (IS_LAYER_ON(NAV)) {
+					// confirm selection of an app (ctrl+cmd) if any
+					unselect_app_selection();
+                	layer_off(NAV);
+				}
+                // if (timer_elapsed(timer_base_nav) < TAPPING_TERM) {
+                //     default_layer_xor((DVO + 1) | (QWE + 1));
+				// 	register_mods(MOD_MASK_CSAG);
+				// 	register_code(KC_SPC);
+                //     clear_keyboard();
+                // }
+            }
+            return false;
+        case T_MEDIA_H_SYM:
+            if (record->event.pressed) {
+                timer_media_sym = timer_read();
+                if (IS_LAYER_OFF(SYM)) layer_on(SYM);
+            } else {
+                if (IS_LAYER_ON(SYM)) {
+                	layer_off(SYM);
+				}
+                // if (timer_elapsed(timer_media_sym) < TAPPING_TERM) {
+                //     layer_on(MEDIA);
+                // }
+            }
+            return false;
+        case BASE:
+            if (record->event.pressed) {
+                layer_clear();
+            }
+            return false;
+        case HIS_BACK:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_GUI);
+				register_code(KC_LBRC);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case HIS_FORW:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_GUI);
+				register_code(KC_RBRC);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case PREV_TAB:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_CS);
+				register_code(KC_TAB);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case NEXT_TAB:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_CTRL);
+				register_code(KC_TAB);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case PREV_APP:
+            if (record->event.pressed) {
+                if (is_select_prev_app_active == false) {
+                    if (is_select_next_app_active == true) {
+                        is_select_next_app_active = false;
+                        clear_mods();
+                    }
+                    register_mods(MOD_MASK_SG);
+                    tap_code(KC_TAB);
+                    is_select_prev_app_active = true;
+                } else {
+                    tap_code(KC_TAB);
+                }
+            }
+            return false;
+        case NEXT_APP:
+            if (record->event.pressed) {
+                if (is_select_next_app_active == false) {
+                    if (is_select_prev_app_active == true) {
+                        is_select_prev_app_active = false;
+                        clear_mods();
+                    }
+                    register_mods(MOD_MASK_GUI);
+                    tap_code(KC_TAB);
+                    is_select_next_app_active = true;
+                } else {
+                    tap_code(KC_TAB);
+                }
+            }
+            return false;
+        case PREW_SPC:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_CTRL);
+				register_code(KC_LEFT);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case NEXT_SPC:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_CTRL);
+				register_code(KC_RGHT);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case WEB_INSPECTOR:
+            if (record->event.pressed) {
+				register_mods(MOD_MASK_AG);
+				register_code(KC_I);
+            } else {
+                clear_keyboard();
+            }
+            return false;
+        case KC_ENT:
+            if (record->event.pressed) {
+                unselect_app_selection();
+            }
+            return esc_and_enter_handle;
+        case KC_ESC:
+            if (record->event.pressed) {
+                unselect_app_selection();
+            }
+            return esc_and_enter_handle;
+        default:
+            return true;
+    }
+};
